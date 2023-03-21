3237a9918e7fae40c66e2b92ceddd33586780d86
Chithanoori Akhila <70878045+ChithanooriAkhila@users.noreply.github.com>
2023-03-21 15:34:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 
+const addDays = require("date-fns/addDays");
 const app = express();
 
 app.get("/", (req, res) => {
@@ -7,11 +8,15 @@ app.get("/", (req, res) => {
   const day = date.getDate();
   const month = date.getMonth();
   const year = date.getFullYear();
-  const formattedDate = `${day}-${month + 1}-${year}`;
+
+  const dateAfter100Days = addDays(new Date(year, month, day), 100);
+
+  const formattedDate = `${dateAfter100Days.getDate()}/${
+    dateAfter100Days.getMonth() + 1
+  }/${dateAfter100Days.getFullYear()}`;
   res.send(formattedDate);
-  //   console.log(formattedDate);
 });
 
-app.listen(3000);
+app.listen(3002);
 
 module.exports = app;
